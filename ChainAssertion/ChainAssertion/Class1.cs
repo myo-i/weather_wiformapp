5a4fd450cc6dc3354a3932fefa36b4af17328979
myo-i <Imiyo0521@gmail.com>
2022-10-31 23:37:25 +0900

    ✨ ChainingAssertionによる簡単なテストの追加

---
+++ ChainAssertion/ChainAssertion/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainAssertion
+{
+    public class Class1
+    {
+        public static int Add(int a, int b)
+        {
+            if (a < 0 )
+            {
+                throw new InputException("負の数は入力できません");
+            }
+            return a + b;
+        }
+    }
+}
---
+++ ChainAssertion/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using ChainAssertion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+//using NUnit.Framework;
+using System;
+using System.CodeDom;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, Class1.Add(1, 2));
+            Class1.Add(1, 2).Is(3);
+
+            var ex = AssertEx.Throws<InputException>(() => Class1.Add(-1, 2));
+            Assert.AreEqual("負の数は入力できません", ex.Message);
+            ex.Message.Is("負の数は入力できません");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void ExceptionTest()
+        {
+            Assert.AreEqual(3, Class1.Add(-1, 2));
+        }
+    }
+}
