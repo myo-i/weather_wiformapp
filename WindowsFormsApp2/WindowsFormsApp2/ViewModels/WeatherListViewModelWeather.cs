f1a10788b1875db86d77f20c0ce9f78cbb6d0ed9
myo-i <Imiyo0521@gmail.com>
2022-11-06 03:21:54 +0900

    ✨ リストのデータに対するViewModel追加

---
+++ WindowsFormsApp2/WindowsFormsApp2/ViewModels/WeatherListViewModelWeather.cs
@@ -0,0 +1,18 @@
+﻿using DDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.ViewModels
+{
+    public sealed class WeatherListViewModelWeather
+    {
+        private WeatherEntity _entity;
+        public WeatherListViewModelWeather(WeatherEntity entity)
+        {
+            _entity = entity;
+        }
+    }
+}
