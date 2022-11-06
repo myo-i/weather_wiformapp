fb75b4afd80124c12abe293541cf29605dea2473
myo-i <Imiyo0521@gmail.com>
2022-11-07 03:07:27 +0900

    ✨ WeatherListViewModelのテスト作成

--- WindowsFormsApp2/DDDTest.Tests/WeatherListViewModelTest.cs
+++ WindowsFormsApp2/DDDTest.Tests/WeatherListViewModelTest.cs
@@ -20,12 +20,14 @@ namespace DDDTest.Tests
 
             entities.Add(new WeatherEntity(
                 1,
+                "東京",
                 Convert.ToDateTime("2018/01/01 12:34:56"),
                 2,
                 12.3f));
 
             entities.Add(new WeatherEntity(
                 2,
+                "神戸",
                 Convert.ToDateTime("2018/01/01 12:34:56"),
                 2,
                 25.21234f));
@@ -34,6 +36,11 @@ namespace DDDTest.Tests
 
             var viewModel = new WeatherListViewModel(weatherMock.Object);
             viewModel.Weathers.Count.Is(2);
+            viewModel.Weathers[0].AreaId.Is("1");
+            viewModel.Weathers[0].AreaName.Is("東京");
+            viewModel.Weathers[0].DateData.Is("2018/01/01 12:34:56");
+            viewModel.Weathers[0].Condition.Is("曇り");
+            viewModel.Weathers[0].Temperature.Is("12.30 ℃");
         }
     }
 }
