1c8c69f1855f81c135a9fb486856e2cc6416e791
myo-i <Imiyo0521@gmail.com>
2022-11-07 23:38:56 +0900

    ✨ リスト（一覧）を表示するためのViewを作成

---
+++ WindowsFormsApp2/WindowsFormsApp2/Views/WeatherListView.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp2.Views
+{
+    partial class WeatherListView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WeatherDataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.WeatherDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WeatherDataGrid
+            // 
+            this.WeatherDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.WeatherDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WeatherDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.WeatherDataGrid.Name = "WeatherDataGrid";
+            this.WeatherDataGrid.RowTemplate.Height = 21;
+            this.WeatherDataGrid.Size = new System.Drawing.Size(622, 515);
+            this.WeatherDataGrid.TabIndex = 0;
+            // 
+            // WeatherListView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 515);
+            this.Controls.Add(this.WeatherDataGrid);
+            this.Name = "WeatherListView";
+            this.Text = "WeatherListView";
+            ((System.ComponentModel.ISupportInitialize)(this.WeatherDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView WeatherDataGrid;
+    }
+}
