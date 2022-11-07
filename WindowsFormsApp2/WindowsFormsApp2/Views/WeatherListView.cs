5f7335a1941a3f669e543ef2877c7b33a4f43037
myo-i <Imiyo0521@gmail.com>
2022-11-07 23:40:25 +0900

    ✨ リスト（一覧）で表示するデータのバインディング

---
+++ WindowsFormsApp2/WindowsFormsApp2/Views/WeatherListView.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using WindowsFormsApp2.ViewModels;
+
+namespace WindowsFormsApp2.Views
+{
+    public partial class WeatherListView : Form
+    {
+        private WeatherListViewModel _viewModel 
+            = new WeatherListViewModel();
+        public WeatherListView()
+        {
+            InitializeComponent();
+
+            // WeatherDataGridはWindowsformで生成した画面の(Name)にあたるところ
+            WeatherDataGrid.DataBindings.Add(
+                "DataSource", _viewModel, nameof(_viewModel.Weathers));
+        }
+    }
+}
