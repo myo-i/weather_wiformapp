940c02663adb5f16e9362217fbd293863ee27122
myo-i <Imiyo0521@gmail.com>
2022-11-08 23:34:36 +0900

    ✨ WeatherSaveViewModelのテスト作成

---
+++ WindowsFormsApp2/DDDTest.Tests/WatherSaveViewModelTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WindowsFormsApp2.ViewModels;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WatherSaveViewModelTest
+    {
+        [TestMethod]
+        public void WeatherRegist()
+        {
+            var viewModel = new WeatherSaveViewModel();
+            viewModel.SelectedAreaId.IsNull();
+            viewModel.DateDataValue.Is(
+                Convert.ToDateTime("2022/01/01 12:34:56"));
+            viewModel.SelectedCondition.Is(1);
+            viewModel.TemperatureText.Is("");
+
+        }
+    }
+}
