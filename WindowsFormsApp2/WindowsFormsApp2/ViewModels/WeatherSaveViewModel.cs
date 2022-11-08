cd9636002d87343e9840fdc58a36eaf6ec99ae9b
myo-i <Imiyo0521@gmail.com>
2022-11-08 23:35:12 +0900

    ✨ WeatherSavaViewModelのクラスとプロパティ作成

---
+++ WindowsFormsApp2/WindowsFormsApp2/ViewModels/WeatherSaveViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WindowsFormsApp2.ViewModels
+{
+    public class WeatherSaveViewModel : ViewModelBase
+    {
+        public WeatherSaveViewModel()
+        {
+            DateDataValue = DateTime.Now;
+        }
+        // AreaIdとConditionはコンボボックスのValueを
+        // バインディングするのでobject型
+        public object SelectedAreaId { get; set; }
+        public DateTime DateDataValue { get; set; }
+        public object SelectedCondition { get; set; }
+        public string TemperatureText { get; set; }
+    }
+}
