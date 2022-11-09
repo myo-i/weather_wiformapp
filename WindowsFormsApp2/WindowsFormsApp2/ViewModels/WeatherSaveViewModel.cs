f2d3f75ab1104f3b8c7c1cef694b2bdeffeb5a7d
myo-i <Imiyo0521@gmail.com>
2022-11-09 22:28:21 +0900

    ✨ Conditionのテスト→Conditionのプロパティ→ConditionのToList

--- WindowsFormsApp2/WindowsFormsApp2/ViewModels/WeatherSaveViewModel.cs
+++ WindowsFormsApp2/WindowsFormsApp2/ViewModels/WeatherSaveViewModel.cs
@@ -1,15 +1,29 @@
-﻿using DDD.Domain.ValueObjects;
+﻿using DDD.Domain.Entities;
+using DDD.Domain.Repositories;
+using DDD.Domain.ValueObjects;
 using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace WindowsFormsApp2.ViewModels
 {
     public class WeatherSaveViewModel : ViewModelBase
     {
-        public WeatherSaveViewModel()
+        private IAreasRepository _areas;
+
+        public WeatherSaveViewModel(IAreasRepository areas)
         {
+            _areas = areas;
+
             DateDataValue = GetDateTime();
             SelectedCondition = Condition.Sunny.Value;
             TemperatureText = String.Empty;
+
+            foreach (var area in _areas.GetData())
+            {
+                Areas.Add(new AreaEntity(area.AreaId, area.AreaName));
+            }
         }
         // AreaIdとConditionはコンボボックスのValueを
         // バインディングするのでobject型
@@ -17,6 +31,11 @@ namespace WindowsFormsApp2.ViewModels
         public DateTime DateDataValue { get; set; }
         public object SelectedCondition { get; set; }
         public string TemperatureText { get; set; }
-
+        public BindingList<AreaEntity> Areas { get; set; }
+                = new BindingList<AreaEntity>();
+        // あくまでConditionが欲しいのはデータの中身ではなく、
+        // 入力した際の区分を返す
+        public BindingList<Condition> Conditions { get; set; }
+        = new BindingList<Condition>(Condition.ToList());
     }
 }
