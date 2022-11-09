099108fadcdc338f1e0b46b149266611cfc00e78
myo-i <Imiyo0521@gmail.com>
2022-11-09 22:25:04 +0900

    ✨ Areasのテスト→viewModelにプロパティとコンストラクタ追加→モック追加

---
+++ WindowsFormsApp2/DDDTest.Tests/WeatherSaveViewModelTest.cs
@@ -0,0 +1,39 @@
+﻿using DDD.Domain.Entities;
+using DDD.Domain.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using WindowsFormsApp2.ViewModels;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherSaveViewModelTest
+    {
+        [TestMethod]
+        public void WeatherRegist()
+        {
+            var areasMock = new Mock<IAreasRepository>();
+            var areas = new List<AreaEntity>();
+            areas.Add(new AreaEntity(1, "東京"));
+            areas.Add(new AreaEntity(2, "神戸"));
+            areasMock.Setup(x => x.GetData()).Returns(areas);
+
+            var viewModelMock = 
+                new Mock<WeatherSaveViewModel>(areasMock.Object);
+            viewModelMock.Setup(x => x.GetDateTime()).Returns(
+                Convert.ToDateTime("2022/01/01 12:34:56"));
+
+            var viewModel = viewModelMock.Object;
+            viewModel.SelectedAreaId.IsNull();
+            viewModel.DateDataValue.Is(
+                Convert.ToDateTime("2022/01/01 12:34:56"));
+            viewModel.SelectedCondition.Is(1);
+            viewModel.TemperatureText.Is("");
+
+            viewModel.Areas.Count.Is(2);
+            viewModel.Conditions.Count.Is(4);
+        }
+    }
+}
