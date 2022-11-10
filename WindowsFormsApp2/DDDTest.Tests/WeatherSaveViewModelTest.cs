6a17d2bdae6237a69532291e974d752114a25ced
myo-i <Imiyo0521@gmail.com>
2022-11-11 00:04:30 +0900

    ✨ Saveのためのモックを作成

--- WindowsFormsApp2/DDDTest.Tests/WeatherSaveViewModelTest.cs
+++ WindowsFormsApp2/DDDTest.Tests/WeatherSaveViewModelTest.cs
@@ -15,14 +15,16 @@ namespace DDDTest.Tests
         [TestMethod]
         public void WeatherRegist()
         {
+            var weatherMock = new Mock<IWeatherRepository>();
             var areasMock = new Mock<IAreasRepository>();
+
             var areas = new List<AreaEntity>();
             areas.Add(new AreaEntity(1, "東京"));
             areas.Add(new AreaEntity(2, "神戸"));
             areasMock.Setup(x => x.GetData()).Returns(areas);
 
             var viewModelMock = 
-                new Mock<WeatherSaveViewModel>(areasMock.Object);
+                new Mock<WeatherSaveViewModel>(weatherMock.Object,areasMock.Object);
             viewModelMock.Setup(x => x.GetDateTime()).Returns(
                 Convert.ToDateTime("2022/01/01 12:34:56"));
 
@@ -43,6 +45,22 @@ namespace DDDTest.Tests
             ex = AssertEx.Throws<InputException>(() => viewModel.Save());
             ex.Message.Is("温度を入力してください");
 
+            viewModel.TemperatureText = "12.345";
+
+            weatherMock.Setup(x => x.Save(It.IsAny<WeatherEntity>())).
+                Callback<WeatherEntity>(returnValue =>
+                {
+                    returnValue.AreaId.Value.Is(2);
+                    returnValue.DateData.Is(
+                        Convert.ToDateTime("2022/01/01 12:34:56"));
+                    returnValue.Condition.Value.Is(1);
+                    returnValue.Temperature.Value.Is(12.345f);
+                });
+
+            viewModel.Save();
+
+            // 下記の記述の場合、weatherMockでテストしようとしたヤツは全て通ったかどうかをチェックしてくれる
+            weatherMock.VerifyAll();
 
         }
     }
