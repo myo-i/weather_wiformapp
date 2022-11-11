44710ab9a6c12bd304d06acbe97cd780d44a133f
myo-i <Imiyo0521@gmail.com>
2022-11-12 04:07:57 +0900

    ✨ "℃"を表すプロパティのテスト

--- WindowsFormsApp2/DDDTest.Tests/WeatherSaveViewModelTest.cs
+++ WindowsFormsApp2/DDDTest.Tests/WeatherSaveViewModelTest.cs
@@ -34,6 +34,7 @@ namespace DDDTest.Tests
                 Convert.ToDateTime("2022/01/01 12:34:56"));
             viewModel.SelectedCondition.Is(1);
             viewModel.TemperatureText.Is("");
+            viewModel.TemperatureUnitName.Is("℃");
 
             viewModel.Areas.Count.Is(2);
             viewModel.Conditions.Count.Is(4);
